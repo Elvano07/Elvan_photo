9f76cf68720bc3aaf9d68ea9da64829f5b99b3fc
Insomnius <aliana.arib@gmail.com>
2018-01-09 22:06:28 +0700

    Photo by date

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,9 @@
 
 @section('content')
     <div class='container-fluid'>
-    {{-- @foreach($photos as $key0 => $value0) --}}
+    @foreach($photos as $key0 => $value0)
 
-   {{-- @foreach($value0 as $key1 => $value1)
+    @foreach($value0 as $key1 => $value1)
     <div class='row'>
             <div class='col-md-12'>
                 <h4>{{ $bulan[$key0][$key1]['bulan'] }} {{ $bulan[$key0]['tahun'] }}</h4>
@@ -21,26 +21,26 @@
                         <div class="box box-default">
                             <div class="box-body">
                                 <div class='hovereffect'>
-                                    <img class='img-responsive' src='/storage/{{ $photo->filelocation }}'>
+                                    <img class='img-responsive' src='/storage/{{ $value2->filelocation }}'>
                                     <div class="overlay">
                                         <h2>Info</h2>
-                                        <a class="info" style='cursor: pointer' href="photo/{{ $photo->id }}">Click here</a>
+                                        <a class="info" style='cursor: pointer' href="photo/{{$value2->id}}">Click here</a>
                                     </div>
                                 </div>
                                 
                                 <div class='row'>
                                 <div class='col-md-12'>
-                                    <h4>{{ $photo->title }}</h4>
-                                    <p>{{ $photo->description }}</p>
+                                    <h4>{{ $value2->title }}</h4>
+                                    <!-- <p>{{ $value2->description }}</p> -->
                                     <hr>
                                     @php
-                                         $tag = explode(',', $photo->tag); 
+                                         $tag = explode(',', $value2->tag); 
                                     @endphp
                                     <table>
-                                        <tr><td width='25px'><span class='fa fa-map-marker'></span></td>    <td width='80px'><b>Location</b></td>   <td width='30px' class='text-center'>:</td>     <td>{{ $photo->location }}</td></tr>
-                                        <tr><td width='25px'><span class='fa fa-list'></span></td>          <td width='80px'><b>Category</b></td>   <td width='30px' class='text-center'>:</td>     <td>{{ $photo->categories->name }}</td></tr>
-                                        <tr><td width='25px'><span class='fa fa-tag'></span></td>           <td width='80px'><b>Tag</b></td>        <td width='30px' class='text-center'>:</td>     <td>@foreach($tag as $key3 => $value3) <span class='label label-primary'>{{$value3}}</span> @endforeach</td></tr>
-                                        <tr><td width='25px'><span class='fa fa-calendar'></span></td>      <td width='80px'><b>Upload Date</b></td>   <td width='30px' class='text-center'>:</td>  <td>{{ $photo->created_at }}</td></tr>
+                                        <!-- <tr><td width='25px'><span class='fa fa-map-marker'></span></td>    <td width='80px'><b>Location</b></td>   <td width='30px' class='text-center'>:</td>     <td>{{ $value2->location }}</td></tr>
+                                        <tr><td width='25px'><span class='fa fa-list'></span></td>          <td width='80px'><b>Category</b></td>   <td width='30px' class='text-center'>:</td>     <td>{{ $value2->categories->name }}</td></tr>
+                                        <tr><td width='25px'><span class='fa fa-tag'></span></td>           <td width='80px'><b>Tag</b></td>        <td width='30px' class='text-center'>:</td>     <td>@foreach($tag as $key3 => $value3) <span class='label label-primary'>{{$value3}}</span> @endforeach</td></tr> -->
+                                        <tr><td width='25px'><span class='fa fa-calendar'></span></td>      <td width='80px'><b>Upload Date</b></td>   <td width='30px' class='text-center'>:</td>  <td>{{ $value2->created_at }}</td></tr>
                                     </table>
                                 </div>
                                 </div>
@@ -48,7 +48,7 @@
                         </div>
                     </div>
                     
-                    @if($counter % 3 == 2 || $photo == end($value1)) </div> @endif
+                    @if($counter % 3 == 2 || $value2 == end($value1)) </div> @endif
                     @php
                     $counter++;
                     @endphp
@@ -56,66 +56,9 @@
                 @endforeach
             </div>
         </div>
-    @endforeach --}}
-
-    <div class='row'>
-        <div class='col-md-12'>
-            <!-- <h4>$bulan[$key0][$key1]['bulan'] $bulan[$key0]['tahun']</h4> -->
-                <div class='row'>
-                    @foreach($photo as $photo)                 
-                        <div class='col-md-4 col-sm-6' style="height: 430px; box-sizing: border-box; margin: 10px 0;">
-                            <div class="box box-default">
-                                <div class="box-body">
-                                    <div class='hovereffect'>
-                                        <img class='img-responsive' src="/storage/{{ $photo->filelocation }}" style="height: 200px">
-                                        <div class="overlay">
-                                            <h2>Info</h2>
-                                            <a class="info" style='cursor: pointer' href="photo/{{ $photo->id }}">Click here</a>
-                                        </div>
-                                    </div>
-                                    
-                                    <div class='row'>
-                                        <div class="box-body">
-                                            <div class='col-md-12' style="height: 200px;">
-                                                <h4>{{ $photo->title }}</h4>
-                                              <!--  <p>{{ $photo->description }}</p>-->
-                                                <hr>
-                                                <table>
-                                                  <!--  <tr>
-                                                        <td width='25px'><span class='fa fa-map-marker'></span></td>
-                                                        <td width='80px'><b>Location</b></td>
-                                                        <td width='30px' class='text-center'>:</td>
-                                                        <td>{{ $photo->location }}</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td width='25px'><span class='fa fa-list'></span></td>
-                                                        <td width='80px'><b>Category</b></td>
-                                                        <td width='30px' class='text-center'>:</td>
-                                                        <td>{{ $photo->categories_id }}</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td width='25px'><span class='fa fa-tag'></span></td>
-                                                        <td width='80px'><b>Tag</b></td>
-                                                        <td width='30px' class='text-center'>:</td>
-                                                        <td><span class='label label-primary'>{{ $photo->tag }}</span></td>
-                                                    </tr>-->
-                                                    <tr>
-                                                        <td width='25px'><span class='fa fa-calendar'></span></td>
-                                                        <td width='80px'><b>Upload Date</b></td>
-                                                        <td width='30px' class='text-center'>:</td>
-                                                        <td>{{ $photo->created_at }}</td>
-                                                    </tr>
-                                                </table>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    @endforeach
-                </div>
-            </div>
-        </div>
+    @endforeach
+    
+    @endforeach
     </div>
 @endsection
 
